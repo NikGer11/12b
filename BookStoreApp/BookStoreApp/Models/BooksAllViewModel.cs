04459fb827d8f9c9ef008f783ec77fcee3ffcaab
alextakov <alekscvetanov5@gmail.com>
2023-02-17 10:20:49 +0200

    yes

--- BookStoreApp/BookStoreApp/Models/BooksAllViewModel.cs
+++ BookStoreApp/BookStoreApp/Models/BooksAllViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookStoreApp.Models
 {
-    public class BooksAllViewModel
+    public class BookAllViewModel
     {
+        public int Id { get; set; }
+
+        [Display(Name = "BookName")]
+        public string Name { get; set; }
+
+        [Display(Name = "Author")]
+        public string Author { get; set; }
+
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Picture")]
+        public string Picture { get; set; }
+
+        public DateTime YearOfPublication { get; set; }
+
+        public double Price { get; set; }
     }
 }
